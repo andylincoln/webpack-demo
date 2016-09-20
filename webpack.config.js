da7efb52607bd914ad7aa80c35db627fe65382a8
Andy Lincoln <andrewlincoln11@gmail.com>
2016-09-20 16:39:57 -0400

    Automatic Browser Refresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const validate = require("webpack-validator");
+const parts = require('./libs/parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -36,7 +37,14 @@ switch(process.env.npm_lifecycle_event) {
     break;
 
   default:
-    config = merge(common, {});
+  config = merge(
+   common,
+   parts.devServer({
+     // Customize host/port here if needed
+     host: process.env.HOST,
+     port: process.env.PORT
+   })
+ );
 
 }
 
