c6147108e19b617ee5054eaf67d6504d0340bb67
Andy Lincoln <andrewlincoln11@gmail.com>
2016-09-21 17:25:44 -0400

    Eliminating Unused CSS

--- app/component.js
+++ app/component.js
@@ -5,7 +5,6 @@ module.exports = function () {
   var element = document.createElement("h1");
   element.innerHTML = "Hello, Andy!";
   // Attach the generated class name
-  element.className = styles.redButton;
-
+  element.className = 'pure-button';
   return element;
 };
