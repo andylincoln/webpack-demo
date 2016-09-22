7c10f8c5ba8b0685dfc981267716bbd1bdb9fd08
Andy Lincoln <andrewlincoln11@gmail.com>
2016-09-21 20:43:45 -0400

    Fixing the environment variables code

--- libs/parts.js
+++ libs/parts.js
@@ -95,6 +95,17 @@ exports.extractBundle = function(options) {
   };
 }
 
+exports.setFreeVariable = function(key, value) {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  };
+}
+
 exports.clean = function(path) {
   return {
     plugins: [
